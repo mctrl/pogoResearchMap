8f9bcd0f9e900d1eab3f148f2be82ce6b4fb8491
nadeemakbar786 <nadeemakbar786@gmail.com>
2018-09-03 09:46:52 -0400

    new pin icon

--- js/google-sheet.js
+++ js/google-sheet.js
@@ -42,7 +42,7 @@ function showPosition(position) {
     draggable: true,
     animation: google.maps.Animation.DROP,
     title: 'Your location',
-    icon: 'icons/pin2.png'
+    icon: 'icons/pin1.png'
   });
   map.setCenter(thePosition);
   map.setZoom(17);
